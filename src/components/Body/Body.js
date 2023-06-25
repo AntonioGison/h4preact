9b090632043f7fc209575bb8696d9648294ca997
Antonio Gison <antonio@gison.it>
2023-06-25 15:29:23 +0100

    navbar + body responsive completed

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,9 +2,55 @@ import React from "react";
 import "./Body.css";
 
 function Body() {
-    return (
-        <h1>hello</h1>
-    );
+  return (
+    <div className="background">
+      <img className="body_hero"
+        src={require("./hp_first_left.svg").default}
+        alt="Hack4pizza awards"
+        width="500"
+      />
+      <div className="body_right">
+          <p className="font">Hack. Win. Collect.</p>
+    <form>
+         <input className="body_input" type="text" id="first-name" name="first-name" placeholder="First Name" required />
+         <input  className="body_input" type="text" id="last-name" name="last-name" placeholder="Last Name" required />
+        <br/>
+        <input className="body_input"  type="email" id="email" name="email" placeholder="Email Address" required />
+         <br/>
+        <input className="body_input"  type="password" id="password" name="password" placeholder="Password" required />
+         <input  className="body_input" type="password" id="confirm-password" name="confirm-password" placeholder="Confirm Password" required />
+        <br/>
+        <input className="body_input"  type="text" id="city" name="city" placeholder="City" required />
+        <br/>
+        <div className="body_terms">
+        <input type="checkbox" id="terms" name="terms" value="terms"/>
+        <label for="terms">Accept Terms & Conditions</label>
+        </div>
+        <br />
+        <input  className="body_btn_yellow" type="submit" value="SIGN UP" />
+        <div class="body_social">
+          <span className="body_before"></span>
+          <span className="body_social_text">OR SIGN UP WITH</span>
+          <span className="body_after"></span>
+        </div>
+        <div className="body_social_icons">
+        <img
+        src={require("./login_with_facebook.svg").default}
+        alt="Hack4pizza facebook login"
+      />
+      <img
+        src={require("./login_with_linkedin.svg").default}
+        alt="Hack4pizza linkedin login"
+      />
+        <img
+        src={require("./login_with_github.svg").default}
+        alt="Hack4pizza github login"
+      />
+        </div>
+    </form>
+      </div>
+    </div>
+  );
 }
 
 export default Body;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,19 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav>
-      <a href="changeitlater" className="logo">
+      <a href="changeitlater" className="nav_logo">
         <img src={require("./logo.svg").default} alt="Hack4Pizza" />
       </a>
-      <div className="right">
-        <input type="email" placeholder="Enter Email address" />
+      <div className="nav_right">
+        <input className="nav_input" type="email" placeholder="Enter Email address" />
 
-        <div className="container">
-          <div className="passcontainer">
-            <input type="password" placeholder="Enter Password" />
+        <div className="nav_container">
+          <div className="nav_passcontainer">
+            <input className="nav_input" type="password" placeholder="Enter Password" />
             <a href="changeitlater">Forgot Password?</a>
           </div>
-          <button>LOGIN</button>
-          <button className="mobilebutton">SIGN UP</button>
+          <button className="nav_button">LOGIN</button>
+          <button className="nav_mobilebutton">SIGN UP</button>
         </div>
       </div>
     </nav>
